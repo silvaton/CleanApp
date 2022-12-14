e3fe5e62f526c7c7372a3aebb00b1b2cfb75b922
Ton Silva <silvatonsp@gmail.com>
2022-12-14 19:07:20 -0300

    move production code to their own files

---
+++ Data/HttpPostClient.swift
@@ -0,0 +1,12 @@
+//
+//  HttpPostClient.swift
+//  Data
+//
+//  Created by Ton Silva on 14/12/22.
+//
+
+import Foundation
+
+public protocol HttpPostClient {
+    func post(to url: URL, with data: Data?)
+}
---
+++ Data/RemoteAddAccount.swift
@@ -0,0 +1,23 @@
+//
+//  RemoteAddAccount.swift
+//  Data
+//
+//  Created by Ton Silva on 14/12/22.
+//
+
+import Foundation
+import Domain
+
+public final class RemoteAddAccount {
+    private let url: URL
+    private let httpClient: HttpPostClient
+    
+    public init(url: URL, httpClient: HttpPostClient) {
+        self.url = url
+        self.httpClient = httpClient
+    }
+    
+    public func add(addAccountModel: AddAccountModel) {
+        httpClient.post(to: url, with: addAccountModel.toData())
+    }
+}
