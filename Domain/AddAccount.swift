1eeff55b634d812f2752dd84f19758c2ba651a04
Ton Silva <silvatonsp@gmail.com>
2022-12-14 18:02:37 -0300

    transform domain use case and models to public

--- Domain/AddAccount.swift
+++ Domain/AddAccount.swift
@@ -7,15 +7,15 @@
 
 import Foundation
 
-protocol AddAcount {
+public protocol AddAcount {
     func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, Error>) -> Void)
 }
 
-struct AddAccountModel {
-    var name: String
-    var email: String
-    var password: String
-    var passwordConfirmation: String
+public struct AddAccountModel {
+    public var name: String
+    public var email: String
+    public var password: String
+    public var passwordConfirmation: String
     
 }
 
