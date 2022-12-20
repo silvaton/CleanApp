bc664d56813e69195ecc8c177ca6d7f7a72c6a7b
Ton Silva <silvatonsp@gmail.com>
2022-12-20 20:13:24 -0300

    ensure RemoteAddAccount completes with error when HttpClient completes with error

---
+++ Data/Http/HttpError.swift
@@ -0,0 +1,12 @@
+//
+//  HttpError.swift
+//  Data
+//
+//  Created by Ton Silva on 20/12/22.
+//
+
+import Foundation
+
+public enum HttpError: Error {
+    case noConnectivity
+}
---
+++ Domain/Errors/DomainError.swift
@@ -0,0 +1,12 @@
+//
+//  DomainError.swift
+//  Domain
+//
+//  Created by Ton Silva on 20/12/22.
+//
+
+import Foundation
+
+public enum DomainError: Error {
+    case unexpected
+}
